d5a595fa3f6f840454a1ad4d4e48e9ebe989bd78
TomislavTomljenovic <tomislavtomljenovic253@gmail.com>
2022-03-17 16:01:43 +0100

    a

---
+++ pios.projekt.API/pios.projekt.models/Models/SchoolClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pios.projekt.models.Models
+{
+    public class SchoolClass
+    {
+        public int Id { get; set; }
+        public string ClassName { get; set; }
+
+        public List<Student> StudentsInClass { get; set; }
+
+    }
+}
