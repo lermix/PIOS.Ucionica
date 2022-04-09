65292082861871ad581b14b03b45373b851a753d
TomislavTomljenovic <tomislavtomljenovic253@gmail.com>
2022-04-09 14:37:34 +0200

    hjhj

---
+++ pios.projekt.API/pios.projekt.models/Models/Exam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pios.projekt.models.Models
+{
+    public class Exam
+    {
+        public int Id { get; set; }
+        public List<Student> students { get; set; } = new List<Student>();
+        public Teacher teacher { get; set; }
+
+        DateTime datum { get; set; } = new DateTime();
+
+        public List<Question> questions { get; set; } = new List<Question>();
+
+    }
+}
